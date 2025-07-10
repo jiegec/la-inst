500a911e975c7d5ede8a0ccc2b30c4cd5a4395d7
Jiajie Chen <c@jia.je>
2025-07-11 00:17:02 +0800

    Improve opcode.rs generation

--- extract_opc.py
+++ extract_opc.py
@@ -2,12 +2,16 @@
 with open('loongarch-opc.c', 'r') as f:
     with open('src/opcode.rs', 'w') as out:
         print('pub const OPCODES: &[(u32, u32)] = &[', file=out)
+        lines = []
         for line in f:
             parts = line.strip().split(',')
             if len(parts[0]) > 1 and parts[0][0] == '{' and parts[0][2] != '&':
                 match_value = int(parts[0][1:], 16)
                 match_mask = int(parts[1][1:], 16)
+                name = parts[2].strip()[1:-1]
                 if match_value == 0:
                     continue
-                print(f'    (0x{match_value:x}, 0x{match_mask:x}),', file=out)
+                lines.append(f'    (0x{match_value:08x}, 0x{match_mask:x}), // {name}')
+        lines = sorted(lines)
+        print('\n'.join(lines), file=out)
         print('];', file=out)
